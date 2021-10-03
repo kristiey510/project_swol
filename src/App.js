921a6849fc968183d017e5c5a75c532156e9436f
Kristie Yip <kyip2@emory.edu>
2021-10-03 00:45:21 -0400

    Add Landing Page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
-import logo from "./logo.svg";
 import "./App.css";
 import { db } from "./firebase";
+import React from "react";
+import {BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Landing from "./pages/Landing";
+import CreateAccount from "./pages/CreateAccount"
+import LogIn from "./pages/LogIn"
 
 function App() {
+  // eslint-disable-next-line
   const handleClick = async () => {
     const ref = db.collection("test").doc("fTsqmwDUuL3RpEGyyxdO");
     const doc = await ref.get();
@@ -12,27 +17,20 @@ function App() {
       console.log("Document data:", doc.data());
     }
   };
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <button type="button" onClick={handleClick}>
-          test query
-        </button>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Landing />
+        </Route>
+        <Route path="/signup" >
+          <CreateAccount />
+        </Route>
+        <Route path="/login" >
+          <LogIn />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,16 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
 import reportWebVitals from './reportWebVitals';
+import { ChakraProvider } from "@chakra-ui/react";
+import "@fontsource/noto-serif"
+import "@fontsource/pacifico"
 
+import App from "./App";
+import customTheme from "./utils/theme";
+
+const rootElement = document.getElementById("root");
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider theme={customTheme}>
+      <App />
+    </ChakraProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  rootElement
 );
-
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
