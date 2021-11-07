64195e6308a61ac67f0eeef950aa58894b90c196
Kristie Yip <kyip2@emory.edu>
2021-11-07 01:31:39 -0400

    Allow profile picture display/change

--- src/pages/ProfileEdit.js
+++ src/pages/ProfileEdit.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react"; 
 import { Link } from "react-router-dom";
 import FeedHeader from "../components/sections/FeedHeader";
-import { getDatabase, ref, onValue } from "firebase/database";
+import { getDatabase, onValue } from "firebase/database";
 import {
   Box,
   Flex,
@@ -16,24 +16,38 @@ import {
   Radio, 
   RadioGroup,
   useColorModeValue,
-  Editable, EditableInput, EditablePreview, IconButton, ButtonGroup, InputGroup, InputLeftAddon
+  Editable,
+ FormLabel,
+ FormControl,
+ Image,
+ InputRightAddon,
+ FormHelperText, EditableInput, EditablePreview, IconButton, ButtonGroup, InputGroup, InputLeftAddon,VisuallyHidden
 } from "@chakra-ui/react";
+import { v4 as uuidv4 } from 'uuid';
 import PropTypes from "prop-types";
-import { doc, setDoc, db, collection, addDoc, updateDoc, auth, getDoc} from "../firebase/firebase";
+import { doc, setDoc, db, collection, addDoc, updateDoc, auth, getDoc, getStorage, uploadBytes, ref, getDownloadURL } from "../firebase/firebase";
 import {EditIcon, CheckIcon} from "@chakra-ui/icons"
 
 
 export default function ProfileEdit(){
 
 const [input, setInput] = useState({name: "", dob: "", gender: "" , weight: "", height_in: "" , height_ft: ""});
-
+const [image , setImage] = useState(null);
+const [Error, setError] = useState("");
 
 useEffect(async () => {
       await getDoc(doc(db, "Profile", auth.currentUser.uid)).then(async(docSnap) => {
         if (docSnap.exists()) {
           const data = await docSnap.data();
           await setInput({name: data.Name,height_in : data.Height_In, height_ft: data.Height_Ft, gender: data.Gender, weight: data.Weight});
-          console.log("Done with data update")
+          const storage = await getStorage();
+          await getDownloadURL(ref(storage, data.Picture_id)).then((url) => {
+            const img = document.getElementById('myimg');
+            img.setAttribute('src', url);
+          }).catch((error) => {
+             console.log(error);
+          });
+          console.log("Done with data update");
         }
        });
     }, [])
@@ -54,7 +68,7 @@ const handleChange = async(name, value) => {
 };
 
 const handleSubmit = async() => {
-   await updateDoc(doc(db, "Profile", "iu90wX9I0cfI7w1KNrMBVfWxFF82"),{
+   await updateDoc(doc(db, "Profile", auth.currentUser.uid),{
       Name: input.name,
       Height_Ft: input.height_ft,
       Height_In: input.height_in,
@@ -63,26 +77,56 @@ const handleSubmit = async() => {
     });
 };
 
+const handleImage = async(event) => {
+  await console.log(event.target.files[0]);
+  await setImage(event.target.files[0]);
+    console.log("hello");
+    // if(await image.size > 5000000){
+    //   console.log("image too large")
+    //   setError("Error: File too large");
+    //   return;
+    // }
+    const acceptedImageTypes = ['image/gif', 'image/jpeg', 'image/png'];
+    if(!acceptedImageTypes.includes(image.type)){
+      console.log("wrong file type:",image.type);
+      setError("Error: Not a JPG or PNG");
+      return;
+    }
+    var filename = uuidv4();
+    const storage = await getStorage();
+    const imageRef = await ref(storage, filename);
+    await uploadBytes(imageRef, image).then((snapshot) => {
+        console.log('Uploaded a blob or file!');
+    });
+    await updateDoc(doc(db, "Profile", auth.currentUser.uid),{
+      Picture_id: filename
+    });
+}
+
 return (
-	<Flex direction="column" align="center" maxW={{ xl: "1200px" }} m="auto" > 
-		<FeedHeader/>
-		<Box rounded={'xl'} mt = "38" w = "680px" h = "600" > 
-			<Heading mt = "5" align = "center" color = "primary.2350"> Profile Update </Heading>
-			<InputGroup mt = "10" ml = "20px">
+	<Flex direction="column" align = "center" maxW={{ xl: "1200px" }} m="auto"> 
+		<FeedHeader />
+    <Flex mt = "30px" ml = "-230px" >
+     <Image id = "myimg" h = "200px" w = "200px" bg = "transparent" mt = "80px" mr = "80px" borderRadius="full"/>
+		<Box rounded={'xl'} mt = "38" w = "680px" h = "600"> 
+			<Heading mt = "-5px" align = "center" color = "primary.2350" > Profile Update </Heading>
+			<InputGroup mt = "60px" ml = "20px">
     			<InputLeftAddon color = "primary.150" fontSize = "md" fontWeight = "extrabold" w = "100px" bg= "primary.3200" ml = "5" mr = "2"> Name </InputLeftAddon>
     			<Editable	
             id = "name"
-            fontSize = "md"
+            fontSize = "lg"
             value = {input.name}
         		isPreviewFocusable={false}
             onChange={(nextValue) => handleChange("name", nextValue)}
             onSubmit = {handleSubmit}
+            color = "orange.400" 
+            fontWeight = "bold"
     			>	
 		      {props => (
 		        <HStack >
-              <EditablePreview textAlign = "center" ml = "3" mt = "1" minW = "420" maxW= "420" minH = "9" maxH = "9"/>
-              <EditableInput textAlign = "center" minW = "420" maxW= "420" minH = "9" maxH = "9"/>
-		          <EditableControls {...props} />
+              <EditablePreview textAlign = "center" ml = "3" mt = "1" minW = "423" maxW= "423" minH = "9" maxH = "9"/>
+              <EditableInput textAlign = "center" minW = "423" maxW= "423" minH = "9" maxH = "9"/>
+              <EditableControls {...props} />
 		        </HStack>
 		      )}
     		</Editable>
@@ -96,11 +140,13 @@ return (
           isPreviewFocusable={false}
           onChange={(nextValue) => handleChange("height_ft", nextValue)}
           onSubmit = {handleSubmit}
+          color = "orange.400" 
+          fontWeight = "bold"
           >  
           {props => (
             <HStack  >
-              <EditablePreview textAlign = "center" ml = "3" mt = "1" minW = "100px" maxW= "100px" minH = "9" maxH = "9"/>
-              <EditableInput variant= "filled" textAlign = "center" minW = "100px" maxW= "100px" minH = "9" maxH = "9"/>
+              <EditablePreview textAlign = "center" ml = "3" mt = "1" minW = "104px" maxW= "104px" minH = "9" maxH = "9"/>
+              <EditableInput variant= "filled" textAlign = "center" minW = "104px" maxW= "104px" minH = "9" maxH = "9"/>
               <EditableControls {...props} />
             </HStack>
           )}
@@ -114,11 +160,13 @@ return (
           isPreviewFocusable={false}
           onChange={(nextValue) => handleChange("height_in", nextValue)}
           onSubmit = {handleSubmit}
+          color = "orange.400" 
+          fontWeight = "bold"
           >  
           {props => (
             <HStack >
-              <EditablePreview textAlign = "center" ml = "3" mt = "1" minW = "100px" maxW = "100px" minH = "9" maxH = "9"/>
-              <EditableInput textAlign = "center" minW = "100px" maxW= "100px" minH = "9" maxH = "9"/>
+              <EditablePreview textAlign = "center" ml = "3" mt = "1" minW = "104px" maxW = "104px" minH = "9" maxH = "9"/>
+              <EditableInput textAlign = "center" minW = "104px" maxW= "104px" minH = "9" maxH = "9"/>
               <EditableControls {...props} />
             </HStack>
           )}
@@ -133,11 +181,14 @@ return (
           isPreviewFocusable={false}
           onChange={(nextValue) => handleChange("weight", nextValue)}
           onSubmit = {handleSubmit}
+          color = "orange.400" 
+          fontWeight = "bold"
           >  
           {props => (
             <HStack >
-              <EditablePreview textAlign = "center" ml = "3" mt = "1" minW = "420" maxW= "420" minH = "9" maxH = "9"/>
-              <EditableInput textAlign = "center" minW = "420" maxW= "420" minH = "9" maxH = "9"/>
+              <EditablePreview textAlign = "center" ml = "3" mt = "1" minW = "385" maxW= "385" minH = "9" maxH = "9"/>
+              <EditableInput textAlign = "center" ml = "3" minW = "385" maxW= "385" minH = "9" maxH = "9"/>
+              <InputRightAddon fontSize = "md" fontWeight = "extrabold" w = "30px"  h = "30px"  > lbs</InputRightAddon>
               <EditableControls {...props}/>
             </HStack>
           )}
@@ -146,20 +197,33 @@ return (
         <InputGroup w = "100px" mt = "10" ml = "20px">
           <InputLeftAddon color = "primary.150" fontSize = "md" fontWeight = "extrabold" w = "100px" bg= "primary.3200" ml = "5" mr = "10"> Gender </InputLeftAddon>
           <RadioGroup color = "primary.2350" fontWeight = "bold" mt = "10px" value= {input.gender} >
-           <HStack spacing="71.5px" >
+           <HStack spacing="73px" >
               <Radio value="Female" size = 'md' onChange={(event) => handleChange("gender", event.target.value)} onClick= {handleSubmit}>Female</Radio>
               <Radio value="Male" size = 'md' onChange={(event) => handleChange("gender", event.target.value)}>Male</Radio>
-              <Radio mr = "0px" value="Other" size = 'md' onChange={(event) => handleChange("gender", event.target.value)}>Other</Radio>
+              <Radio value="Other" size = 'md' onChange={(event) => handleChange("gender", event.target.value)}>Other</Radio>
               <IconButton colorScheme="myblue" aria-label="Edit" size="sm" icon={<CheckIcon />} onClick={handleSubmit} />
            </HStack>
           </RadioGroup>
         </InputGroup>
+        <Box  backgroundColor='tomato'> 
+        </Box>
 
-        <Button ml= "250px" mt = "60px" bg = "primary.3200" color  = "primary.150" fontWeight = "bold" fontSize = "16"> Upload Profile Picture </Button>
+        <FormControl id="email">
+           
+            <FormLabel pt = "12.5px" pl = "18px"ml = "250px" mt= "50px" borderRadius=  "10px" w = "200px" h ="50px" htmlFor="getFile" class="button" bg = "primary.3200" color  = "primary.150" fontWeight = "bold" fontSize = "16">Upload Profile Picture</FormLabel>
+            <Input
+              border = "transparent"
+              type = "file" 
+              onChange = {handleImage}
+              id ="getFile" 
+              style= {{display: "none"}}
+            />
+        </FormControl>
         <Link to='/dashboard'>
         <Button ml= "310px" mt = "25px" bg = "primary.3200" color  = "primary.150" fontWeight = "bold" fontSize = "16"> Done </Button>
         </Link>
 		</Box> 
+    </Flex>
 	</Flex> 
 	);
 };
