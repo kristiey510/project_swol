6b952148d62e3b2fcc0175c23ed20f6f91571bbf
Kristie Yip <kyip2@emory.edu>
2021-11-17 12:46:01 -0500

    fix merge

--- src/components/sections/Topbar/Topbar.jsx
+++ src/components/sections/Topbar/Topbar.jsx
@@ -1,13 +1,8 @@
-<<<<<<< HEAD
-import React, { useState, useEffect } from "react";
-=======
-import React, {useEffect} from "react";
->>>>>>> bffcab197ae2ba3e2b1573caf04af3af0083b2ca
+import React, {useState, useEffect} from "react";
 import { Link } from "react-router-dom";
 import "./Topbar.css";
 import Logo from "../../ui/Logo";
 import { Search } from "@material-ui/icons";
-<<<<<<< HEAD
 import { 
   doc,
   db,
@@ -18,8 +13,6 @@ import {
   ref,
   getDownloadURL,
 } from "../../../firebase/firebase";
-=======
->>>>>>> bffcab197ae2ba3e2b1573caf04af3af0083b2ca
 import {
   Menu,
   MenuButton,
@@ -29,19 +22,6 @@ import {
   MenuDivider,
   Image
 } from "@chakra-ui/react";
-import {
-  doc,
-  setDoc,
-  db,
-  collection,
-  serverTimestamp,
-  getStorage,
-  ref,
-  uploadBytes,
-  auth,
-  getDoc,
-  getDownloadURL
-} from "../../../firebase/firebase";
 
 export default function Topbar() {
   const exit = async () => {
@@ -54,7 +34,6 @@ export default function Topbar() {
   }
 };
 
-<<<<<<< HEAD
 const [image, setImage] = useState(null);
 
  useEffect(() => {
@@ -80,37 +59,6 @@ const [image, setImage] = useState(null);
     getPic();
   }, []);
 
-=======
-export default function Topbar({user}) {
-
-  useEffect(async () => {
-    await getDoc(doc(db, "Profile", user.uid)).then(async (docSnap) => {
-      //console.log("doc: ",docSnap.data())
-      if (docSnap.data().img !== "no_image_provided") {
-
-        const data = docSnap.data();
-
-        //console.log("image", data);
-
-        const img = document.getElementById("propic3");
-        //console.log(img);
-        //get image ref
-        const storage = getStorage();
-        const pathReference = ref(storage, data.Picture_id);
-
-        //download, then set attribute to image tag in file
-        getDownloadURL(pathReference).then((url) => {
-          img.setAttribute("src", url);
-        });
-
-      }
-    });
-  }, []);
-
-
-
-
->>>>>>> bffcab197ae2ba3e2b1573caf04af3af0083b2ca
   return (
     <div className="topbarContainer">
       <div className="topbarLeft">
@@ -129,21 +77,12 @@ export default function Topbar({user}) {
       </div>
       <div className="topbarRight">
         <Menu>
-<<<<<<< HEAD
           <MenuButton as={Button} colorScheme="transparent" w = "50px" p = "0" pl = "5px">
             <Image
               borderRadius = "50%"
               w = "40px"
               h = "40px"
               id = "myimg" 
-=======
-          <MenuButton as={Button} colorScheme="transparent">
-            <img
-              id = "propic3"
-              src=""
-              alt="Just a dude"
-              className="topbarImg"
->>>>>>> bffcab197ae2ba3e2b1573caf04af3af0083b2ca
             />
           </MenuButton>
           <MenuList bg="#FDEBD0" color="primary.2500" minW="150px" maxW="150px">
